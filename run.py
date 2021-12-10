d094b5b9da1c7ec4f57434a59c2650f0b9706449
ibromurph <ibrahim.murphy@hotmail.com>
2021-12-10 13:47:37 +0000

    Added structure/functions of game to code

--- run.py
+++ run.py
@@ -2,3 +2,26 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+#imports
+
+
+#contstants
+
+
+# Game class
+
+#init
+
+#print boards
+
+#create ships
+
+#check if all ships destroyed
+
+#get computer move
+
+#start game
+
+#exit game
+
+#main menu
